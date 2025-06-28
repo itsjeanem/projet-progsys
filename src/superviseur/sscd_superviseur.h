5596162819b8d7cc737e3e842bb37b0de2daee6a
debeadama <debe.soro@uvci.edu.ci>
2025-06-28 13:56:59 +0000

    Mise à jour superviseur

--- src/superviseur/sscd_superviseur.h
+++ src/superviseur/sscd_superviseur.h
@@ -1,9 +1,23 @@
-#ifndef SUPERVISEUR_H
-#define SUPERVISEUR_H
+#ifndef SSCD_SUPERVISEUR_H
+#define SSCD_SUPERVISEUR_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <syslog.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 #include <pthread.h>
+#include <sys/stat.h>
 
-// Configuration système
+// Structure de configuration
 typedef struct
 {
     char log_file[256];
@@ -11,16 +25,16 @@ typedef struct
     int port;
 } Config;
 
-// Déclaration des variables globales
-extern Config config;
-extern volatile int shutdown_flag;
-extern volatile int running;
-
-// Fonctions publiques
+// Déclarations de fonctions
 void handle_signal(int signum);
-void init_config();
+void init_config(void);
 void log_event(const char *level, const char *message);
-void *listen_alerts_from_moniteur();
+void display_connected_clients(void);
+void cleanup_terminated_clients(void);
+void add_client(int socket_fd, struct sockaddr_in *client_addr, pid_t pid);
+void remove_client(pid_t pid);
+void *client_monitor_thread(void *arg);
+void *listen_alerts_from_moniteur(void *arg);
 void handle_client(int client_socket);
 
-#endif // SUPERVISEUR_H
+#endif // SSCD_SUPERVISEUR_H
